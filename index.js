7aff6be81c2125e45728f5fbd75e6e46239e09dd
08-albert <claudiuaalbert@gmail.com>
2023-06-14 11:55:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+
+
+// function openForm(){
+//     document.getElementById('myForm').style.display="block"
+// }
+
+// function closeForm(){
+//     document.getElementById('myForm').style.display="none"
+// }
+
+document.querySelector("show-login").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".popup" .close-btn).addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+});
